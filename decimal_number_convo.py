0598f7eb25595438f1926525f8c9bd06aca5daee
the-cross-art <imrannaz326@gmail.com>
2020-08-11 19:22:05 +0530

    Updated Hexa-Challenge

--- decimal_number_convo.py
+++ decimal_number_convo.py
@@ -1,2 +1,15 @@
 for i in range(17):
     print("{0:>2} in binary is {0:>08b}".format(i))
+
+#Printing in hexa decimal
+for i in range(17):
+    print("{0:>2} in hex is {0:>02x}".format(i))
+
+x = 0x20
+y = 0x0a
+
+print(x)
+print(y)
+print(x * y)
+
+print(0b0010101010)
