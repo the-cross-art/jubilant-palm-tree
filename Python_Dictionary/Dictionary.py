dfe7d36c5c2d6f1a73752215ed48f31e6ad704dd
the-cross-art <imrannaz326@gmail.com>
2020-08-13 19:21:04 +0530

    End of dictionaries pt-3

--- Python_Dictionary/Dictionary.py
+++ Python_Dictionary/Dictionary.py
@@ -20,12 +20,45 @@ print(fruit["lemon"])
 # fruit.clear()
 # cant use del fruit as ir creates an error
 print(fruit)
-while True:
-    dic_key = input("Please enter a fruit")
-    if dic_key == "quit":
-        break
-    if dic_key in fruit:
-        description = fruit.get(dic_key)
-        print(description)
-    else:
-        print("we Dont have a " + dic_key)
+# while True:
+#     dic_key = input("Please enter a fruit")
+#     if dic_key == "quit":
+#         break
+#     description = fruit.get(dic_key, "we dont have a " + dic_key)
+#     print(description)
+    # if dic_key in fruit:
+    #     description = fruit.get(dic_key)
+    #     print(description)
+    # else:
+    #     print("we Dont have a " + dic_key)
+
+
+# for snack in fruit:
+#     print(fruit[snack])
+
+# for i in range(10):
+#     for snack in fruit:
+#         print(snack + " is " + fruit[snack])
+#     print('-'*40)
+
+# ordered_keys = list(fruit.keys())
+# ordered_keys.sort()
+# ordered_keys = sorted(list(fruit.keys()))
+# for f in ordered_keys:
+#     print(f + " = " + fruit[f])
+
+for f in sorted(fruit.keys()):
+    print(f + " = " + fruit[f])
+
+# not an very efficent way to
+# for val in fruit.values():
+#     print(val)
+#
+# for key in fruit:
+#     print(fruit[key])
+
+fruit_keys = fruit.keys()
+print(fruit_keys)
+
+fruit["tomato"] = "not nice with ice cream"
+print(fruit_keys)
